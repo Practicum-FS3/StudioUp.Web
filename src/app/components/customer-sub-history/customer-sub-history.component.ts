aa97ee5128ae56ef4746c879d7d07ce58f1528b5
Tamar-Amar <amtamar747@gmail.com>
2024-08-01 15:31:09 +0300

    AddingSunscriptionCustomerComponent

--- src/app/components/customer-sub-history/customer-sub-history.component.ts
+++ src/app/components/customer-sub-history/customer-sub-history.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerSubscription } from '../../models/customerSubscription';
+import { CustomerSubscriptionService } from '../../services/customer-subscription.service';
 
 @Component({
   selector: 'app-customer-sub-history',
@@ -10,10 +11,23 @@ export class CustomerSubHistoryComponent implements OnInit {
   subscriptions: CustomerSubscription[] = [];
 
   ngOnInit() {
+    console.log('customer-sub-history intered');
     this.subscriptions = [
       new CustomerSubscription(1, 101, 5, new Date('2023-01-01')),
       new CustomerSubscription(2, 102, 6, new Date('2023-02-01')),
       new CustomerSubscription(3, 454, 7, new Date('2023-06-01')),
     ];
   }
+
+  //רק מי שעובד לה החיבור מפה לדאטה והוא מלא בטבלה של CustomerSubscription
+  // שתשתמש בזה- זה מביא נתונים מש כמובן
+
+  // constructor(private subscriptionService: CustomerSubscriptionService) { }
+  // ngOnInit() {
+  //   console.log('server customer-sub-history intered');
+  //   this.subscriptionService.getAll().subscribe(data => {
+  //     this.subscriptions = data;
+  //     console.log(data);
+  //   })
+  // }
 }
