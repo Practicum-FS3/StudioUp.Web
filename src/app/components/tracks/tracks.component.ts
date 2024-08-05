03c16828d268c34ba6dd425cc3174c4cdea4a2b6
Tamar-Amar <amtamar747@gmail.com>
2024-08-05 13:44:37 +0300

    updating-track-import

--- src/app/components/tracks/tracks.component.ts
+++ src/app/components/tracks/tracks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Track } from '../../models/track';
+import { Track } from '../../models/Track';
 import { TracksService } from '../../services/tracks/tracks.service';
 
 @Component({
