8a1a5d06cef26abbe849c20fe388b1046cacd280
Gila Kenigsberg <g0527160283@gmail.com>
2024-09-02 22:33:21 +0300

    using address service

--- src/app/models/customerSubscription.ts
+++ src/app/models/customerSubscription.ts
@@ -1,10 +1,8 @@
 export class CustomerSubscription {
-    constructor(
-      public id: number,               // מזהה מנוי ללקוח (PK)
-      public customerId: number,      // מזהה לקוח (FK)
-      public subscriptionTypeId: number, // מזהה מנוי (FK)
-      public startDate: Date         // תאריך התחלה
-    ) {}
-  
-  }
-  
+  constructor(
+    public id: number, // מזהה מנוי ללקוח (PK)
+    public customerId: number, // מזהה לקוח (FK)
+    public subscriptionTypeId: number, // מזהה מנוי (FK)
+    public startDate: Date // תאריך התחלה
+  ) {}
+}
--- src/app/services/address/address.service.ts
+++ src/app/services/address/address.service.ts
@@ -1,22 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AddressService {
+  private citiesUrl: string =
+    'https://data.gov.il/api/3/action/datastore_search?resource_id=5c78e9fa-c2e2-4771-93ff-7f400a12f7ba&fields=שם_ישוב';
+  private streetsUrl: string =
+    'https://data.gov.il/api/3/action/datastore_search?resource_id=9ad3862c-8391-4b2f-84a4-2d4c68625f4b&fields=שם_רחוב';
 
-  private citiesUrl: string = 'https://data.gov.il/api/3/action/datastore_search?resource_id=5c78e9fa-c2e2-4771-93ff-7f400a12f7ba';
-  private streetsUrl: string = 'https://data.gov.il/api/3/action/datastore_search?resource_id=9ad3862c-8391-4b2f-84a4-2d4c68625f4b';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getCities(): Observable<any> {
-    return this.http.get<any>(this.citiesUrl);
+  getCities(value: string): Observable<string[]> {
+    return this.http
+      .get<any>(this.citiesUrl)
+      .pipe(
+        map((response) =>
+          response.result.records.filter((c: string ) =>
+            c.startsWith(value)
+          )
+        )
+      );
   }
 
-  getStreets(settlementSymbol: string): Observable<any> {
-    return this.http.get<any>(`${this.streetsUrl}?settlementSymbol=${settlementSymbol}`);
+  getStreets(value: string, city?: string): Observable<string[]> {
+    if (!city) {
+      return this.http
+        .get<any>(this.streetsUrl)
+        .pipe(
+          map((response) =>
+            response.result.records.filter((s: string ) =>
+              s.startsWith(value)
+            )
+          )
+        );
+    }
+    return this.http
+      .get<any>(`${this.streetsUrl}&filters={"שם_ישוב":"${city}"}`)
+      .pipe(
+        map((response) =>
+          response.result.records.filter((s: string ) =>
+            s.startsWith(value)
+          )
+        )
+      );
   }
 }
